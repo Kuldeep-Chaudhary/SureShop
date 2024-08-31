101c0fb35b6e018872d669aef60631214eeb1ab3
kuldeep <kuldeep@manthanitsolutions.in>
2024-08-31 14:28:19 +0530

    first commit

--- src/pages/dashboard/AddProducts.jsx
+++ src/pages/dashboard/AddProducts.jsx
@@ -3,56 +3,123 @@ import DashboardFooter from '../../components/dashboard/DashboardFooter';
 import DashboardNavbar from './DashboardNavbar';
 import { ParentContain } from '../../styles/dashboard';
 import { useUIContext } from "../../context/ui";
-import { Button, FormControl, Grid, TextField, Typography, } from '@mui/material';
-
+import { Button, FormControl, Grid, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 
 const AddProducts = () => {
   const { barActive } = useUIContext();
 
-  const [dinpute, setdinpute] = useState({pname:'',pcategory:'',psize:'',pprice:'',pupload:'',pdescription:''});
+  const [dinpute, setdinpute] = useState({
+    pname: '',
+    pcategory: '',
+    psize: '',
+    pprice: '',
+    pupload: null,
+    pdescription: ''
+  });
 
-  const inphendle = (e)=>{
-    const {name,value}=e.target
-    setdinpute({...dinpute,[name]:value})
-  }
+  const inphendle = (e) => {
+    const { name, value, files } = e.target;
 
+    if (name === 'pupload') {
+      setdinpute({ ...dinpute, [name]: files[0] });
+    } else {
+      setdinpute({ ...dinpute, [name]: value });
+    }
+  };
 
-const postproduct = async(e)=>{
-  const response = await axios.post('http://localhost:9999/admin/addproducts',dinpute)
-  // setfnwaknf(response.data)
-}
+  const postproduct = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+
+      for (const key in dinpute) {
+        formData.append(key, dinpute[key]);
+      }
+
+      for (let [key, value] of formData.entries()) {
+        console.log(`${key}:`, value);
+      }
+
+      const response = await axios.post('http://localhost:9999/admin/addproducts', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+
+      console.log(response.data.msg);
+      setdinpute('')
+    } catch (error) {
+      console.error("Error posting product:", error);
+    }
+  };
 
   return (
     <>
       <ParentContain barActive={barActive}>
-      <DashboardNavbar />
+        <DashboardNavbar />
         <Typography variant='h4' my={3} textAlign="center">Add New Products</Typography>
 
         <Grid container spacing={{ xs: 2, sm: 2, md: 2, lg: 2 }} p={3}>
           <Grid item xs={12} sm={6} md={6}>
             <FormControl fullWidth>
-              <TextField id="pname" label="Product Name" variant="outlined" value={dinpute.pname} name='pname' onChange={inphendle}/>
+              <TextField
+                id="pname"
+                label="Product Name"
+                variant="outlined"
+                value={dinpute.pname}
+                name='pname'
+                onChange={inphendle}
+              />
             </FormControl>
           </Grid>
           <Grid item xs={12} sm={6} md={6}>
             <FormControl fullWidth>
-              <TextField id="pcategory" label="Product Category" variant="outlined" value={dinpute.pcategory} onChange={inphendle} name='pcategory'/>
+              <TextField
+                id="pcategory"
+                label="Product Category"
+                variant="outlined"
+                value={dinpute.pcategory}
+                onChange={inphendle}
+                name='pcategory'
+              />
             </FormControl>
           </Grid>
           <Grid item xs={12} sm={4} md={4}>
             <FormControl fullWidth>
-              <TextField id="psize" label="Product Size" variant="outlined" value={dinpute.psize}  onChange={inphendle} name='psize'/>
+              <TextField
+                id="psize"
+                label="Product Size"
+                variant="outlined"
+                value={dinpute.psize}
+                onChange={inphendle}
+                name='psize'
+              />
             </FormControl>
           </Grid>
           <Grid item xs={12} sm={4} md={4}> 
             <FormControl fullWidth>
-              <TextField id="pprice" label="Product Price" variant="outlined" value={dinpute.pprice} onChange={inphendle} name='pprice'/>
+              <TextField
+                id="pprice"
+                label="Product Price"
+                variant="outlined"
+                value={dinpute.pprice}
+                onChange={inphendle}
+                name='pprice'
+              />
             </FormControl>
           </Grid>
           <Grid item xs={12} sm={4} md={4}> 
             <FormControl fullWidth>
-              <TextField id="pupload" label="Upload image" type='file' InputLabelProps={{shrink: true,}} variant="outlined" value={dinpute.pupload} onChange={inphendle} name='pupload'/>
+              <TextField
+                id="pupload"
+                label="Upload Image"
+                type='file'
+                InputLabelProps={{ shrink: true }}
+                variant="outlined"
+                onChange={inphendle}
+                name='pupload'
+              />
             </FormControl>
           </Grid>
           <Grid item xs={12}>
@@ -63,9 +130,9 @@ const postproduct = async(e)=>{
                 multiline
                 rows={4}
                 variant="outlined"
-                fullWidth
-                onChange={inphendle} name='pdescription'
-                 value={dinpute.pdescription}
+                value={dinpute.pdescription}
+                onChange={inphendle}
+                name='pdescription'
               />
             </FormControl>
           </Grid>
@@ -89,7 +156,3 @@ const postproduct = async(e)=>{
 };
 
 export default AddProducts;
-
-
-
-// https://ride-store-newdemo.myshopify.com/pages/contact-us-1
--- src/pages/dashboard/DProducts.jsx
+++ src/pages/dashboard/DProducts.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import DashboardFooter from '../../components/dashboard/DashboardFooter'
 import DashboardNavbar from './DashboardNavbar'
 import { Box, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material'
 import { CartImage } from '../../styles/cart'
-import { products } from '../../data'
+// import { products } from '../../data'
 import { ParentContain } from '../../styles/dashboard'
 import { useUIContext } from "../../context/ui";
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const DProducts = () => {
   const {barActive} = useUIContext();
+  const [Product,setProduct]= useState([])
+
+const getdata = async(e)=>{
+const response = await axios.get('http://localhost:9999/getProduct')
+setProduct(response.data)
+}
+const imageBaseUrl = process.env.REACT_APP_IMAGE_BASE_URL;
+
+useEffect(()=>{
+getdata()
+},[])
+
+console.log(Product,'Product');
+
   return (
     <>
     <ParentContain barActive={barActive}>
@@ -33,66 +48,24 @@ const DProducts = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            <TableRow>
-            <TableCell>
-            <Box sx={{width:"100%",minWidth:"250px",display:"flex"}}><CartImage src={products[0].images}/></Box>
-            </TableCell>
-            <TableCell>Bag</TableCell>
-            <TableCell>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, incidunt!</TableCell>
-            <TableCell>XL</TableCell>
-            <TableCell>$5.8</TableCell>
-            <TableCell>Male</TableCell>
-            <TableCell><IconButton LinkComponent={Link} to='/admin/editdata'><EditIcon/></IconButton></TableCell>
-            <TableCell><IconButton ><DeleteIcon/></IconButton></TableCell>
-            </TableRow>
-            <TableRow>
-            <TableCell>
-            <Box sx={{width:"100%",minWidth:"250px",display:"flex"}}><CartImage src={products[0].images}/></Box>
-            </TableCell>
-            <TableCell>Bag</TableCell>
-            <TableCell>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, incidunt!</TableCell>
-            <TableCell>XL</TableCell>
-            <TableCell>$5.8</TableCell>
-            <TableCell>Male</TableCell>
-            <TableCell><IconButton LinkComponent={Link} to='/admin/editdata'><EditIcon/></IconButton></TableCell>
-            <TableCell><IconButton ><DeleteIcon/></IconButton></TableCell>
-            </TableRow>
-            <TableRow>
-            <TableCell>
-            <Box sx={{width:"100%",minWidth:"250px",display:"flex"}}><CartImage src={products[0].images}/></Box>
-            </TableCell>
-            <TableCell>Bag</TableCell>
-            <TableCell>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, incidunt!</TableCell>
-            <TableCell>XL</TableCell>
-            <TableCell>$5.8</TableCell>
-            <TableCell>Male</TableCell>
-            <TableCell><IconButton LinkComponent={Link} to='/admin/editdata'><EditIcon/></IconButton></TableCell>
-            <TableCell><IconButton ><DeleteIcon/></IconButton></TableCell>
-            </TableRow>
-            <TableRow>
-            <TableCell>
-            <Box sx={{width:"100%",minWidth:"250px",display:"flex"}}><CartImage src={products[0].images}/></Box>
-            </TableCell>
-            <TableCell>Bag</TableCell>
-            <TableCell>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, incidunt!</TableCell>
-            <TableCell>XL</TableCell>
-            <TableCell>$5.8</TableCell>
-            <TableCell>Male</TableCell>
-            <TableCell><IconButton LinkComponent={Link} to='/admin/editdata'><EditIcon/></IconButton></TableCell>
-            <TableCell><IconButton ><DeleteIcon/></IconButton></TableCell>
-            </TableRow>
-            <TableRow>
-            <TableCell>
-            <Box sx={{width:"100%",minWidth:"250px",display:"flex"}}><CartImage src={products[0].images}/></Box>
-            </TableCell>
-            <TableCell>Bag</TableCell>
-            <TableCell>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, incidunt!</TableCell>
-            <TableCell>XL</TableCell>
-            <TableCell>$5.8</TableCell>
-            <TableCell>Male</TableCell>
-            <TableCell><IconButton LinkComponent={Link} to='/admin/editdata'><EditIcon/></IconButton></TableCell>
-            <TableCell><IconButton ><DeleteIcon/></IconButton></TableCell>
-            </TableRow>
+            {Product.map((item,i)=>{
+              const {pupload, pname, pcategory, psize, pprice, pdescription} = item;
+              return (
+                <TableRow key={i}>
+                <TableCell>
+                <Box sx={{width:"100%",minWidth:"250px",display:"flex"}}>
+                  <CartImage src={`${imageBaseUrl}/${pupload}`}/></Box>
+                </TableCell>
+                <TableCell>{pname}</TableCell>
+                <TableCell>{pcategory}</TableCell>
+                <TableCell>{psize}</TableCell>
+                <TableCell>{pprice}</TableCell>
+                <TableCell>{pdescription}</TableCell>
+                <TableCell><IconButton LinkComponent={Link} to='/admin/editdata'><EditIcon/></IconButton></TableCell>
+                <TableCell><IconButton ><DeleteIcon/></IconButton></TableCell>
+                </TableRow>
+              )
+            })}
           </TableBody>
         </Table>
       </TableContainer>
